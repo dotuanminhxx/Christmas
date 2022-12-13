870482011550b3d53fbe8d1c098c809b40792705
dotuanminhxx <112957357+dotuanminhxx@users.noreply.github.com>
2022-12-13 20:31:13 +0700

    Christmas

---
+++ BTVN/app.js
@@ -0,0 +1,10 @@
+let user = document.getElementById("user").value;
+console.log(user);
+
+function test(){
+    if(user === "minh"){
+        console.log('dung');
+    }else{
+        console.log('sai');
+    }
+}
